65457387b83a4183cdc951d8a5fd25cad7e216de
Harish <aharish604@gmail.com>
2020-12-09 22:39:10 +0530

    package name changed

---
+++ app/src/main/java/com/codewithharish/allinonestatussaver/api/APIServices.java
@@ -0,0 +1,36 @@
+package com.codewithharish.allinonestatussaver.api;
+
+import com.codewithharish.allinonestatussaver.model.TiktokModel;
+import com.codewithharish.allinonestatussaver.model.TwitterResponse;
+import com.codewithharish.allinonestatussaver.model.story.FullDetailModel;
+import com.codewithharish.allinonestatussaver.model.story.StoryModel;
+import com.google.gson.JsonObject;
+
+import java.util.HashMap;
+
+import io.reactivex.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
+
+public interface APIServices {
+    @GET
+    Observable<JsonObject> callResult(@Url String Value, @Header("Cookie") String cookie, @Header("User-Agent") String userAgent);
+
+    @FormUrlEncoded
+    @POST
+    Observable<TwitterResponse> callTwitter(@Url String Url, @Field("id") String id);
+
+    @POST
+    Observable<TiktokModel> getTiktokData(@Url String Url, @Body HashMap<String, String> hashMap);
+
+    @GET
+    Observable<StoryModel> getStoriesApi(@Url String Value, @Header("Cookie") String cookie, @Header("User-Agent") String userAgent);
+
+    @GET
+    Observable<FullDetailModel> getFullDetailInfoApi(@Url String Value, @Header("Cookie") String cookie, @Header("User-Agent") String userAgent);
+}
---
+++ app/src/main/java/com/codewithharish/allinonestatussaver/interfaces/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.codewithharish.allinonestatussaver.interfaces;
+
+
+import com.codewithharish.allinonestatussaver.model.story.TrayModel;
+
+public interface UserListInterface {
+    void userListClick(int position, TrayModel trayModel);
+}
